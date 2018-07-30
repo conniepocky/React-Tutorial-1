f4542357fd8e4cb43631cbd32075d46e11c32b34
conniepocky <c.amer@icloud.com>
2018-07-30 11:42:28 +0100

    added files

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Waffles are nice! They are the best!!</Text>
+        <Text style={styles.maintext}>Waffles are nice! They are the best!!</Text>
+        <Image
+          style={styles.mainImage}
+          source={{
+            uri: "https://www.wellplated.com/wp-content/uploads/2016/01/Blender-Whole-Wheat-Waffles-Recipe.jpg"
+         }}
+        />
+
+      <Button
+          onPress={this._moreWaffle}
+          title="Click me to learn more about waffles!"
+        />
+
       </View>
     );
+
   }
+
+  _moreWaffle = () => {
+    Alert.alert("Go and get waffles now they are amazing!!!")
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +37,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  mainImage: {
+    borderColor: "#6171A7",
+    borderWidth: 10,
+    width: 350,
+    height: 350
+  },
+
+  maintext: {
+    color: "steelblue"
+  }
 });
